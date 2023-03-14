aa136556ef3ac30ada8b038f1d5f4c04d99c93e3
Eymen Varilci <evarilci@gmail.com>
2023-03-14 10:16:07 +0300

    Initial commit - Project created, Storyboard deleted and still works, assets imported,

---
+++ CharmAI/CharmAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CharmAI
+//
+//  Created by Eymen Varilci on 14.03.2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+

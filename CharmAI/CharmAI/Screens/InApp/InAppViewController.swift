705255170183ad9a0b32d63e097dfa09c2d63e8f
Eymen Varilci <evarilci@gmail.com>
2023-03-16 11:42:53 +0300

    deleted unused viewModel funcs

---
+++ CharmAI/CharmAI/Screens/InApp/InAppViewController.swift
@@ -0,0 +1,21 @@
+//
+//  InAppViewController.swift
+//  CharmAI
+//
+//  Created by Eymen Varilci on 16.03.2023.
+//
+
+import UIKit
+
+final class InAppViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    
+}
